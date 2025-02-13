deb342718dc96c3c02c3a8ab6b0a2e20005d1e6e
tahminabithy <tahmina.akter9519@gmail.com>
2025-02-13 18:47:27 -0500

    removed node modules and dist

--- dist/modules/comments/comments.route.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.commentsRoute = void 0;
-const express_1 = require("express");
-const comments_controller_1 = require("./comments.controller");
-exports.commentsRoute = (0, express_1.Router)();
-exports.commentsRoute.post("/comments", comments_controller_1.commentController.createComment);
-// commentsRoute.get("/comments", commentController.getComments);
--- dist/modules/users/users.route.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.userRoute = void 0;
-const express_1 = require("express");
-const users_controller_1 = require("./users.controller");
-exports.userRoute = (0, express_1.Router)();
-exports.userRoute.post("/register", users_controller_1.userController.createUser);
-exports.userRoute.get("/users/:id", users_controller_1.userController.getSingleUser);
